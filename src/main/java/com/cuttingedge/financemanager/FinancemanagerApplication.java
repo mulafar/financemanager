663ec057b7760db6b54765bcb96f7a40f7c776cc
mulafar-p@hpe.com <mulafar-p@hpe.com>
2019-07-19 13:03:51 +0530

    source code structure-first commit-repo creation

---
+++ src/main/java/com/cuttingedge/financemanager/FinancemanagerApplication.java
@@ -0,0 +1,20 @@
+package com.cuttingedge.financemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FinancemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancemanagerApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello world";
+	}
+}
